4f209aeabb7668e144e87306dc1cff3629c09c8f
kazu <gambal.job@gmail.com>
2019-06-15 17:26:46 +0900

    シーン遷移

---
+++ SPAJAM_TOKAI/Assets/Source/ButtonFunc.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonFunc : MonoBehaviour
+{
+    //ホームに行く
+    public void HomeScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void MyPageScene()
+    {
+        SceneManager.LoadScene("MyPage");
+    }
+
+    public void FindUmbrellaScene()
+    {
+        SceneManager.LoadScene("FindUmbrella");
+    }
+
+    public void TalkScene()
+    {
+        SceneManager.LoadScene("Talk");
+    }
+}
